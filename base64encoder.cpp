21dfd4b6c5735e5be16218296ef643003fe55bcf
Nathan Files <nfiles2009@gmail.com>
2013-08-14 08:46:36 -0400

    basic things

--- base64encoder.cpp
+++ base64encoder.cpp
@@ -6,12 +6,14 @@
 static char* Base64Encoder::encode ( const string& input ) {
 	string bit_pattern = "";
 
+	char c;
 	for (string::iterator i = input.begin(); i != input.end(); i++)
 	{
-
+		c = *i;
+		for (int j = 0; j < 8; ++j) {
+			bit_pattern += (char)((c >> j) & 1);
+		}
 	}
 }
-
-string 
 
 #endif // BASE64ENCODER_H
--- main.cpp
+++ main.cpp
@@ -1,8 +1,18 @@
 #include <iostream>
+using std::cout;
+using std:: endl;
+
+#include <string>
+using std::string;
+
+#include "base64encoder.h"
 
 int main(int argc, char** argv) {
+	Base64Encoder base64 = new Base64Encoder();
 
-
+	string hello = "Hello, world!";
+	
+	cout << base64.encode(hello);
 
 	return 0;
 }
