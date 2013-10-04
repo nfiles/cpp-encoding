109e5e87bbec457a3e086ec785cdd8919ba1ac4f
Nathan Files <nfiles2009@gmail.com>
2013-10-04 14:51:30 -0400

    don't even remember what these changes are

--- test.sh
+++ test.sh
@@ -4,7 +4,9 @@ printf "\n> Building...\n"
 if make build; then
 	if [ -a ./bin/encoding.exe ]; then
 		printf "\n> Running...\n"
-		./bin/encoding.exe 'Hello, World!' 'My name is Nathan.' 'Lorem Ipsum dolor sit amet'
+		./bin/encoding.exe 'Hello, World!'\
+						   'My name is Nathan.'\
+						   'Lorem Ipsum dolor sit amet'
 	else
 		printf "\n> File not found (./bin/encoding.exe)\n"
 	fi
