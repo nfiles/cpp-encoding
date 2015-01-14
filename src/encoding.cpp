07000a6a054f8be5e9fd622d0f8066cf9c232620
Nathan Files <nfiles2009@gmail.com>
2015-01-13 19:47:14 -0500

    code clean-up - remove unused commented code - remove unused using/include statements - remove exe

--- src/encoding.cpp
+++ src/encoding.cpp
@@ -9,16 +9,13 @@ using std::string;
 
 template<class EncodingScheme>
 class Encoding {
-public:
-	string encode ( const string& input ) {
-		return EncodingScheme::encode(input);
-	}
-	string decode ( const string& input ) {
-		return EncodingScheme::decode(input);
-	}
-
-protected:
-	static string Base64Hash;
+ public:
+  string encode ( const string& input ) {
+    return EncodingScheme::encode(input);
+  }
+  string decode ( const string& input ) {
+    return EncodingScheme::decode(input);
+  }
 };
 
-#endif
+#endif // ENCODING
--- src/main.cpp
+++ src/main.cpp
@@ -6,44 +6,34 @@ using std::endl;
 using std::string;
 
 #include "base64encoding.h"
-// #include "encoding.h"
 
 int main(int argc, char *argv[]) {
-	string * inputs;
-	int inputs_len;
+  string * inputs;
+  int inputs_len;
 
-	Encoding<Base64Encoding> base64;
+  Encoding<Base64Encoding> base64;
 
-	if (argc > 1) {
-		inputs = new string[argc - 1];
-		inputs_len = argc - 1;
-		for (int i = 0; i < inputs_len; ++i) {
-			inputs[i] = argv[i + 1];
-		}
-	} else {
-		inputs = new string[1];
-		inputs_len = 1;
-		inputs[0] = "Hello, World!";
-	}
+  if (argc > 1) {
+    inputs = new string[argc - 1];
+    inputs_len = argc - 1;
+    for (int i = 0; i < inputs_len; ++i) {
+      inputs[i] = argv[i + 1];
+    }
+  } else {
+    inputs = new string[1];
+    inputs_len = 1;
+    inputs[0] = "Hello, World!";
+  }
 
-	string encoded;
-	for (int i = 0; i < inputs_len; ++i) {
-		cout << "Clear:   " << inputs[i] << endl;
-		cout << "Encoded: " << (encoded = base64.encode(inputs[i])) << endl;
-		cout << "Decoded: " << base64.decode(encoded) << endl;
-		cout << endl;
-	}
+  string encoded;
+  for (int i = 0; i < inputs_len; ++i) {
+    cout << "Clear:   " << inputs[i] << endl;
+    cout << "Encoded: " << (encoded = base64.encode(inputs[i])) << endl;
+    cout << "Decoded: " << base64.decode(encoded) << endl;
+    cout << endl;
+  }
 
-	// cout << "A: " << (int)'A' << endl
-	// 	 << "Z: " << (int)'Z' << endl
-	// 	 << "a: " << (int)'a' << endl
-	// 	 << "z: " << (int)'z' << endl
-	// 	 << "0: " << (int)'0' << endl
-	// 	 << "9: " << (int)'9' << endl
-	// 	 << "+: " << (int)'+' << endl
-	// 	 << "/: " << (int)'/' << endl;
+  delete [] inputs;
 
-	delete [] inputs;
-
-	return 0;
+  return 0;
 }
